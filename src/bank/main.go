f83c2867f72755c413d934bba5a71c8ca71f6a4c
davipythonweb <galaxys3tab32@gmail.com>
2024-08-10 00:23:37 -0300

    create contas correntes `aula 2`

---
+++ src/bank/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var titular string = "Eliote"
+	var numeroAgencia int = 589
+	var numeroConta int = 123456
+	var saldo float64 = 125.5
+
+	fmt.Println(titular, numeroAgencia, numeroConta, saldo)
+
+	var titular2 string = "Mariah"
+	var numeroAgencia2 int = 745
+	var numeroConta2 int = 324567
+	var saldo2 float64 = 215.5
+
+	fmt.Println(titular2, numeroAgencia2, numeroConta2, saldo2)
+
+}
