08bd3b37a08191d5ab36a3cd649f34d25151ffcc
davipythonweb <galaxys3tab32@gmail.com>
2024-08-17 03:16:34 -0300

    populando valores ao struct

--- src/bank/main.go
+++ src/bank/main.go
@@ -10,5 +10,11 @@ type ContaCorrente struct {
 }
 
 func main() {
-	fmt.Println(ContaCorrente{})
+	contaDoEliote := ContaCorrente{titular: "Eliote", numeroAgencia: 589,
+		numeroConta: 11525, saldo: 125.5}
+
+	contaDaMariah := ContaCorrente{"Mariah", 222, 24321, 200}
+
+	fmt.Println(contaDoEliote)
+	fmt.Println(contaDaMariah)
 }
