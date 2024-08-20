ede2fe661c268d9e56544bc4287369f28175c18d
davipythonweb <galaxys3tab32@gmail.com>
2024-08-20 00:11:57 -0300

    criando uma nova struct

--- src/bank/contas/contaCorrente.go
+++ src/bank/contas/contaCorrente.go
@@ -1,7 +1,11 @@
 package contas
 
+import (
+	"Go_bank_account__/src/bank/clientes"
+)
+
 type ContaCorrente struct {
-	Titular       string
+	Titular       clientes.Titular
 	NumeroAgencia int
 	NumeroConta   int
 	Saldo         float64
